abfabd5f9abf9a5c6928b78957164461f054c843
AndresCalvoi <andres.calvo@instaleap.io>
2023-11-01 20:25:09 -0500

    add typescript files for db and api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar/Navbar";
 
 const Home = () => {
+  const [products, setProducts] = useState<TProduct[]>([]);
+
+  useEffect(() => {
+    fetch("/api/avo")
+      .then((response) => response.json())
+      .then((json) => {
+        setProducts(json.data);
+      });
+  }, []);
+
   return (
     <div>
+      s
       <Navbar />
       <h1>Home</h1>
+      {products.map((product) => (
+        <div>{product.name}</div>
+      ))}
     </div>
   );
 };

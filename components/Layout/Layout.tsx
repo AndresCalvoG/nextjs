4951170a23b920e93dd08a1ee40b66e122c93ba2
AndresCalvoi <andres.calvo@instaleap.io>
2023-11-13 10:18:33 -0500

    add global css to next js

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -3,7 +3,7 @@ import Navbar from "../Navbar/Navbar";
 
 const Layout = ({ children }) => {
   return (
-    <div>
+    <div className="container">
       <Navbar />
       {children}
       <footer>This is the footer</footer>

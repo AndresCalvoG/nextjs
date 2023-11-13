38d284436744f91e9a8f58457712f5d853bdc034
AndresCalvoi <andres.calvo@instaleap.io>
2023-11-13 10:22:14 -0500

    add css module in nextjs

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
+import style from "./layout.module.css";
 
 const Layout = ({ children }) => {
   return (
-    <div className="container">
+    <div className={style.container}>
       <Navbar />
       {children}
       <footer>This is the footer</footer>
